72ae2e72ce315596e43bf56af69a89079a3bfcc8
DaVinci192 <58537048+DaVinci192@users.noreply.github.com>
2021-02-14 01:54:04 -0500

    Added frontend assets and set express.static to assets folder

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 
 app.use(express.json({limit: '1000mb'}))
 app.use(express.urlencoded({extended:true, limit: '1000mb'}))
+app.use(express.static('assets'))
 app.post("/image/:id", async (req, res) => {
   const body = req.body
   const socketId = req.params.id
